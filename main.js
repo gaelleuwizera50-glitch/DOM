024699ae665e46200fac482d32b72c1f68f77a47
gaelleuwizera50-glitch <gaelleuwizera50@gmail.com>
2026-03-30 13:24:28 +0300

    We added a DOM

---
+++ main.js
@@ -0,0 +1,5 @@
+const mainHeading = document.querySelector('#main-heading');
+console.log(mainHeading);
+
+const introParagraph = document.querySelector(".Intro-paragraphh");
+console.log(introParagraph);
